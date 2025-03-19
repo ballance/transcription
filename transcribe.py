98217bd7a62c2c3f2c983485b12a6ca43c593990
Ballance <ballance@gmail.com>
2025-03-19 10:05:04 -0400

    Adding video processing and updating .gitignore

--- transcribe.py
+++ transcribe.py
@@ -1,13 +1,13 @@
 import whisper
 
-output_file_name = "house-of-sky-and-breath.txt"
+output_file_name = "./transcribed/house-of-sky-and-breath.txt"
 
 # Load the pre-trained Whisper model. Models come in various sizes (tiny, base, small, medium, large).
 # Larger models provide better accuracy but require more computational resources.
-model = whisper.load_model("base")
+model = whisper.load_model("large")
 
 # Transcribe the audio file and store the result
-result = model.transcribe("./house-of-sky-and-breath.wav")
+result = model.transcribe("./house-of-sky-and-breath.wav", verbose=True)
 
 # Save the transcribed text to an output file with UTF-8 encoding
 with open(output_file_name, "w", encoding="utf-8") as txt:
